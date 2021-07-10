41dab72eace623476c4b2f83c53f35cacc191aa0
JoeNemo208 <85529296+JoeNemo208@users.noreply.github.com>
2021-07-09 20:07:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ var c=b-a;
 var mr=Math.random()*c+((b+a)/2)-c/2;
 return mr;
 };
+ function sin(angle) {
+                	return Math.sin(angle / 180 * Math.PI);
+                };
+                
+                // A cosine function that converts from radians to degrees.
+                function cos(angle) {
+                	return Math.cos(angle / 180 * Math.PI);
+                };
+                
+                // An atan2 function.
+                function atan2(x, y) {
+                	return Math.atan2(x, y) / Math.PI * 180;
+                };
+// Returns a number from a certain range.
+                function lerp(num1, num2, amount) {
+                	return num1 + (num2 - num1) * amount;
+                };
+ 
+                // constrains a number to a certain range.
+                function constrain(num, min, max) {
+                	return Math.max(Math.min(num, max), min);
+                };
+                
+                // Finds the distance between two points.
+                function dist(x1, y1, x2, y2) {
+                	return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+                };
+                
