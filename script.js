e76147b6631c4e329d8be8db9ec8bf7176fa2c29
JoeNemo208 <85529296+JoeNemo208@users.noreply.github.com>
2021-08-08 17:41:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1038,16 +1038,17 @@ var jn = function(id, a, b, c) {
     //console.log(jntarget);
     return jns[jntarget];
 };
-var b=1; 
+var b=0; 
   function Ready(x){
 var a=window.setInterval(function(){
+
 if(window.jQuery!==undefined){
 x();
-console.log("Loaded!");
+
 window.clearInterval(a);
 }
  else{
- console.log("Loading.");
+
  }
 },2);
 }
