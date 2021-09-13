19128cde01bfeb0e18db0416a7a5c18a3872d0ab
JoeNemo208 <85529296+JoeNemo208@users.noreply.github.com>
2021-09-13 17:17:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ LoadScript("https://cdnjs.cloudflare.com/ajax/libs/mathjs/9.4.4/math.min.js","bo
 LoadScript("https://cdn.jsdelivr.net/npm/animejs@3.0.1/lib/anime.min.js","body");
     
         /*Load p5.js*/
-LoadScript("https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js","body");
+
     
     /*Load Google Icons*/
 
